9829700882823f1ecf07b73cf8abccc1685df694
PawelLuty <pawel.luty@ncbr.gov.pl>
2026-09-08 22:11:59 +0200

    fix

--- bdot_merge.py
+++ bdot_merge.py
@@ -1,45 +1,22 @@
 import geopandas as gpd
-import pandas as pd
 from pathlib import Path
 
-def normalize_geometry_column(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """Zapewnia, że aktywna kolumna geometrii w GeoDataFrame nazywa się 'geometry'."""
-    current_geom_name = gdf.geometry.name  # Pobiera aktualną nazwę kolumny przestrzennej
-    if current_geom_name != 'geometry':
-        gdf = gdf.rename_geometry('geometry')
-    return gdf.to_crs(epsg=2180)
-
-# Ustawienia
-base_dir = Path(r".\bdot-budynki_powiaty")  # Zmień na swój katalog
+base_dir = Path(r"input")
 teryt = "0663"
-# teryt = "0201"
-years = range(2015, 2025)  # 2015-2024
-years = range(2024, 2025)  # 2015-2024
 
-# 1. Wczytanie pliku bazowego
-base_file = base_dir / f"{teryt}_bud.parquet"
+base_file = base_dir / f"{teryt}_2026_bud.parquet"
 print(f"Wczytywanie pliku bazowego: {base_file.name}")
-gdf_base = normalize_geometry_column(gpd.read_parquet(base_file))
+gdf_base = gpd.read_parquet(base_file)
 
-# Obliczamy powierzchnię obiektów bazowych (wymagane do warunku >90%)
 gdf_base["area_base"] = gdf_base.geometry.area
 
-# Słownik na wyniki dla każdego roku
-results_by_year = {}
-
-for year in years:
+for year in [2024]:
     year_file = base_dir / f"{teryt}_{year}_bud.parquet"
     if not year_file.exists():
         print(f"Pomiędzy: Brak pliku {year_file.name}")
         continue
+    gdf_year = gpd.read_parquet(year_file)
 
-    print(f"\n--- Przetwarzanie roku: {year} ---")
-    gdf_year = normalize_geometry_column(gpd.read_parquet(year_file))
-
-    # print(f"bazowy: {gdf_base.geometry.name}")
-    # print(f"{year}: {gdf_year.geometry.name}")
-    # KROK 1: Dopasowanie po LOKALNYID
-    # Łączymy po LOKALNYID
     merged = gdf_base.merge(
         gdf_year,
         # gdf_year[['LOKALNYID', 'geometry']],
@@ -51,52 +28,45 @@ for year in years:
     # print(merged.columns)
     # Rozdzielamy na znalezione po ID i nieznalezione
     found_by_id = merged[merged['geometry_year'].notna()].copy()
+    for index, row in found_by_id.iterrows():
+        gdf_base.loc[index, "ZABYTEK"] = row['ZABYTEK']
+        gdf_base.loc[index, "UTWORZONO"] = str(row['DATAUTW'])[:4]
+
+    # found_by_id.to_parquet(f"dopasowane_id_{year}.parquet")
     missing_by_id = merged[merged['geometry_year'].isna()].copy()
 
     print(f"Dopasowane po LOKALNYID: {len(found_by_id)} / {len(gdf_base)}")
     print(f"Brak dopasowania po ID (szukanie przestrzenne): {len(missing_by_id)}")
 
     # KROK 2: Dopasowanie przestrzenne dla brakujących (>90% nałożenia)
-    found_by_spatial = []
+    found_by_spatial = 0
 
     if len(missing_by_id) > 0:
         # Odrzucamy kolumnę geometry_year powstałą z merge
         missing_gdf = missing_by_id.drop(columns=['geometry_year'])
 
-        # Spatial Join (s-join) na podstawie nachodzenia obiektów (intersects)
-        # sjoin_neighbor zawiera dopasowania przestrzenne z pliku rocznego
         candidates = gpd.sjoin(
             missing_gdf,
-            gdf_year[['LOKALNYID', 'geometry']],
+            gdf_year[['LOKALNYID', 'geometry', 'DATAUTW','KODKST', 'FUNOGBUD']],
             how='inner',
             predicate='intersects'
         )
-
-        # Obliczamy pole powierzchni części wspólnej (intersection)
-        for idx, row in candidates.iterrows():
+        # candidates.to_parquet(f"dopasowane_{year}.parquet")
+        for index, row in candidates.iterrows():
             geom_base = row.geometry
             # Pobieramy geometrię kandydata z pliku rocznego
-            # print(row)
             geom_year = gdf_year.loc[gdf_year['LOKALNYID'] == row['LOKALNYID_right'], 'geometry'].values[0]
-
             # Obliczenie pola części wspólnej
             intersection_area = geom_base.intersection(geom_year).area
-            overlap_ratio = intersection_area / row['area_base']
-            if overlap_ratio > 0.90:
-                print(f"{overlap_ratio}%: {row}")
-                found_by_spatial.append({
-                    'LOKALNYID_base': row['LOKALNYID_left'],
-                    'LOKALNYID_found': row['LOKALNYID_right'],
-                    'overlap_ratio': overlap_ratio
-                })
+            overlap_ratio = round(intersection_area / row['area_base'],2)
+            if overlap_ratio > 0.95:
+                print(f"{overlap_ratio}%: {intersection_area} {geom_year.area}")
 
-    df_spatial = pd.DataFrame(found_by_spatial)
-    print(f"Dopasowane przestrzennie (>90%): {len(df_spatial)}")
+                gdf_base.loc[index, "ZABYTEK"] = row['ZABYTEK']
+                gdf_base.loc[index, "UTWORZONO"] = str(row['DATAUTW_right'])[:4]
+                found_by_spatial = found_by_spatial + 1
 
-    # Zapis wyniku dla danego roku
-    results_by_year[year] = {
-        'matched_id': found_by_id[['LOKALNYID']],
-        'matched_spatial': df_spatial
-    }
+    print(f"Dopasowane przestrzennie: {found_by_spatial}")
 
 print("\nGotowe! Analiza zakończona.")
+gdf_base.to_parquet(f"bdot_rzb.parquet")
--- bdot_pobranie.py
+++ bdot_pobranie.py
@@ -4,13 +4,23 @@ import tempfile
 import requests
 import geopandas as gpd
 from pathlib import Path
+from datetime import datetime
+from fnmatch import fnmatch
+
+fields_rename = {"X_DATAUTW": "DATAUTW", "LKOND": "LICZ_KONDY" }
+fields_delete = ["X_*", "*_NIL", "NAZWA", "SKROT_KART", "INFO_DODAT", "UWAGI", "PRZES_NAZW", "WERSJA", "POCZ_WERSJA",
+                 "ZRO_DANYCH", "PNAZW", "KOD25K", "KOD50K", "KOD100K", "KOD250K", "KOD500K", "KOD1000K", "KOD1000_NI",
+                 "POCZWERS", "KONIECWERS", "OZNA_ZMIAN", "GMLID", "WERSJAID", "POCZ_WERSJ"]
 
 
 def download_and_convert_bdot(teryt_powiat: str, rok: int, output_dir: str = ".") -> Path:
     teryt_powiat = str(teryt_powiat).zfill(4)
     teryt_woj = teryt_powiat[:2]
 
-    url = f"https://opendata.geoportal.gov.pl/Archiwum/bdot10k/{rok}/SHP/{teryt_woj}/{teryt_powiat}_SHP_{rok}.zip"
+    if rok == datetime.now().year:
+        url = f"https://opendata.geoportal.gov.pl/bdot10k/schemat2021/SHP/{teryt_powiat[:2]}/{teryt_powiat}_SHP.zip"
+    else:
+        url = f"https://opendata.geoportal.gov.pl/Archiwum/bdot10k/{rok}/SHP/{teryt_woj}/{teryt_powiat}_SHP_{rok}.zip"
     output_path = Path(output_dir) / f"{teryt_powiat}_{rok}_bud.parquet"
 
     if output_path.exists():
@@ -43,14 +53,13 @@ def download_and_convert_bdot(teryt_powiat: str, rok: int, output_dir: str = "."
         target_shp = shp_files[0]
         print(f"Znaleziono plik: {target_shp.name}")
 
-        print("Wczytywanie i przetwarzanie GeoPandas...")
+        print("Wczytywanie i przetwarzanie ...")
         gdf = gpd.read_file(target_shp)
 
-        # Usunięcie zbędnego pola ID_IIP
-        # if "ID_IIP" in gdf.columns:
-        #     gdf = gdf.drop(columns=["ID_IIP"])
+        cols_delete = [col for col in gdf.columns if any(fnmatch(col, pattern) for pattern in fields_delete)]
+        print(f"usuwam kolumny: {cols_delete}")
+        gdf = gdf.rename(columns=fields_rename).drop(columns=cols_delete, errors="ignore")
 
-        # Zapis do Parqueta (bez indeksu Pandas)
         print(f"Zapisywanie do: {output_path}")
         gdf.to_parquet(output_path, index=False)
 
@@ -62,10 +71,12 @@ if __name__ == "__main__":
     # Przykład: powiat oswiecimski (1213) lub lubelski (0609), rok 2023 / 2024
     POWIAT = "0663"  # 4-znakowy TERYT powiatu
     # ROK = 2023
-    KATALOG_WYJSCIOWY = r"C:\ProjektyNCBR\podzialShape\bdotarch-budynki_powiaty"
+    KATALOG_WYJSCIOWY = r"input"
 
     Path(KATALOG_WYJSCIOWY).mkdir(parents=True, exist_ok=True)
-    for rok in range(2014,2025):
+    for rok in range(2015,2027):
+    # for rok in range(2026,2027):
+    # for rok in range(2014, 2016):
         try:
             download_and_convert_bdot(teryt_powiat=POWIAT, rok=rok, output_dir=KATALOG_WYJSCIOWY)
         except FileNotFoundError:
